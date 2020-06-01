6ecbb50417877b0a2acea0140fa0d05a24078e64
Roberto <roberto.orellana@glr.pe>
2020-06-01 16:37:13 -0500

    docs: Add Variables

---
+++ zsh.sh
@@ -0,0 +1,17 @@
+#!/bin/zsh
+echo "Hello World!"
+
+#---------------
+# VARIABLES
+#---------------
+MY_MESSAGE="PADORU"
+COPY=${MY_MESSAGE}
+
+echo "$COPY"
+echo "${COPY}"
+echo $COPY
+echo ${COPY}
+echo $NON_EXISTING_VARIABLE
+echo "\
+first line
+second line"
